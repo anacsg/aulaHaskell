5a39208dec35b101d702c9ddf599aa1b69e369ef
anacsg <anacsango@gmail.com>
2019-12-04 20:28:14 +0000

    figurinhas

--- src/Handler/Etc.hs
+++ src/Handler/Etc.hs
@@ -32,7 +32,7 @@ getEtcR = do
             /* Create four equal columns that sits next to each other */
             .column {
               flex: 25%;
-              max-width: 25%;
+              max-width: 10%;
               padding: 0 4px;
             }
             
@@ -42,21 +42,6 @@ getEtcR = do
               width: 100%;
             }
             
-            /* Responsive layout - makes a two column-layout instead of four columns */
-            @media screen and (max-width: 800px) {
-              .column {
-                flex: 50%;
-                max-width: 50%;
-              }
-            }
-            
-            /* Responsive layout - makes the two columns stack on top of each other instead of next to each other */
-            @media screen and (max-width: 600px) {
-              .column {
-                flex: 100%;
-                max-width: 100%;
-              }
-            }
         |]
         [whamlet|
 
