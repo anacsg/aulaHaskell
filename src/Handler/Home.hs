ba4594d34c0e0b29ad351288c5bf84510989a0c5
anacsg <anacsango@gmail.com>
2019-12-04 00:27:35 +0000

    login

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -12,19 +12,6 @@ import Text.Julius
 --import Network.HTTP.Types.Status
 import Database.Persist.Postgresql
 
-getPage2R :: Handler Html 
-getPage2R = do 
-    defaultLayout $ do 
-        $(whamletFile "templates/page2.hamlet")
-
-getPage1R :: Handler Html 
-getPage1R = do 
-    defaultLayout $ do 
-        addStylesheet (StaticR css_bootstrap_css)
-        toWidgetHead $(juliusFile "templates/page1.julius")
-        toWidgetHead $(luciusFile "templates/page1.lucius")
-        $(whamletFile "templates/page1.hamlet")
-
 getHomeR :: Handler Html
 getHomeR = do 
     defaultLayout $ do 
