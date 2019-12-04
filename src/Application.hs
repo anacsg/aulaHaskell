d0e7d74a3bcbe0608ca835c58a86fc8ae186c9c3
anacsg <anacsango@gmail.com>
2019-12-04 00:46:02 +0000

    emailUnico

--- src/Application.hs
+++ src/Application.hs
@@ -41,9 +41,7 @@ import Handler.Common
 import Handler.Home
 import Handler.Usuario
 import Handler.Login
-import Handler.Ator
 import Handler.Serie
-import Handler.Atua
 import Handler.Newsletter
 import Handler.Etc
 
